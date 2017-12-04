78a13f678d8ba39c8ec900c489c4ecd24c0d133e
Lucy <ll7@princeton.edu>
2017-12-04 04:16:42 -0500

    Added chunk to index page

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^team/$', views.team, name='team'),
+	url(r'^pets/$', views.pets, name='pets'),
+	url(r'^volunteer/$', views.volunteer, name='volunteer'),
+	url(r'^donate/$', views.donate, name='donate'),
+	url(r'^contact/$', views.contact, name='contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'website/index.html')
 
+# pet listings
+def pets(request):
+	return render(request, 'website/pets.html')
+
+# home page
+def contact(request):
+	return render(request, 'website/contact.html')
+
+# home page
+def donate(request):
+	return render(request, 'website/donate.html')
+
+# home page
+def volunteer(request):
+	return render(request, 'website/volunteer.html')
+
+# home page
+def team(request):
+	return render(request, 'website/team.html')
+
